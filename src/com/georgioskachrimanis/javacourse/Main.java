1b10196a10b99c1117929f77b7508ec59a60b6a9
gekac <37373492+Bouboukis@users.noreply.github.com>
2021-07-14 10:12:58 +0200

    1

---
+++ src/com/georgioskachrimanis/javacourse/Main.java
@@ -0,0 +1,36 @@
+package com.georgioskachrimanis.javacourse;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int startingNumber = 0, finishingNumber = 999;
+
+        while (startingNumber  < finishingNumber) {
+
+            startingNumber++;
+            if (sumDigits(startingNumber) == -1) {
+                continue;
+            }
+            System.out.println("The digits of number " + startingNumber +
+                    " have a sum of " + sumDigits(startingNumber));
+
+        }
+
+    }
+
+    public static int sumDigits(int number) {
+
+        int sumDigit = 0;
+        if (number < 10 ){
+            return -1;
+        }
+
+        do {
+            int lastDigit  =  number % 10;
+            sumDigit += lastDigit ;
+            number = number / 10;
+        } while(number > 0 );
+
+        return sumDigit;
+    }
+}
